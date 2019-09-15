3f4badd70444cd835467d60079822a010ed1ceee
mattsharker <mattsharker@gmail.com>
2019-09-15 15:23:45 -0700

    Added program to demonstrate looping

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // while loops
+            Console.WriteLine("While loops");
+
+            int x = 0;
+            while (x < 10)
+            {
+                Console.Write(x + " ");
+                ++x;
+            }
+
+
+            // do while
+            Console.WriteLine("\n\nDo While loops");
+
+            x = 0;
+            do
+            {
+                Console.Write(x + " ");
+                ++x;
+            }
+            while (x < 10);
+
+
+            // for loops
+            Console.WriteLine("\n\nFor loops");
+
+            for (int i = 0; i < 10; ++i)
+            {
+                Console.Write(i + " ");
+            }
+
+
+            // exit prompt
+            Console.Write("\n\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
