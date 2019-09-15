230552228daf842d8edfd8b507594a2cf805031d
mattsharker <mattsharker@gmail.com>
2019-09-15 15:16:01 -0700

    Added program to demonstrate how to get user input and convert it

---
+++ UserInput/UserInput/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // getting a line of user unput
+            Console.Write("Enter text: ");
+            string userInput = Console.ReadLine();
+            Console.WriteLine(userInput);
+
+            // getting a non-string from the user
+            Console.Write("Enter an integer to be doubled: ");
+
+            userInput = Console.ReadLine();
+            int userInt = Convert.ToInt32(userInput);
+            userInt *= 2;
+
+            Console.WriteLine(userInput + " doubled is " + userInt);
+
+            // pause until a key is pressed, then exit
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
