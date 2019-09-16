cd0418ca137b03d2b7539febc7b4fc5dc83f820e
mattsharker <mattsharker@gmail.com>
2019-09-16 15:47:06 -0700

    Created program demonstrating generic types

---
+++ Generics/Generics/MyList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+// This clas
+namespace Generics
+{
+    public class MyList<T>  // the "<T>" says that the class has a generic type
+    {
+        private T[] items;  // use "T" as the type throughout the class where it is relevant
+        
+        public MyList()
+        {
+            items = new T[0];
+        }
+
+        public T GetItem(int index)
+        {
+            return items[index];
+        }
+
+        public void Add(T newItem)
+        {
+            T[] newItems = new T[items.Length + 1];
+
+            for (int i = 0; i < items.Length; ++i)
+                newItems[i] = items[i];
+
+            newItems[newItems.Length - 1] = newItem;
+
+            items = newItems;
+        }
+    }
+
+}
---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating an instance of an object using generics
+            MyList<string> strings = new MyList<string>();    // the "<string>" is mandatory, as it uses generic types
+
+            // now, only strings can be added to the list
+            strings.Add("String 1");
+            strings.Add("String 2");
+            //strings.Add(5); // this will cause an error
+            Console.WriteLine("The second element of the list is: " + strings.GetItem(1));
+
+            // example using dictionaries, which has 2 generic types
+            Dictionary<string, int> phoneBook = new Dictionary<string, int>();
+
+            phoneBook["Gates, Bill"] = 5550001;
+            phoneBook["Zuckerberg, Mark"] = 5550002;
+
+            int billsNumber = phoneBook["Gates, Bill"];
+
+            // exit prompt
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
