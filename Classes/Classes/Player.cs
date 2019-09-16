ab8287503390d58f41ce05c49a9ccb078de43502
mattsharker <mattsharker@gmail.com>
2019-09-15 17:05:25 -0700

    Added simple Player class to demonstrate user made classes

---
+++ Classes/Classes/Player.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Classes
+{
+    class Player
+    {
+        private string name;
+
+        private int score;
+        private int livesLeft;
+
+        // constructors
+        public Player(string name)
+        {
+            this.name = name;   // the "this." diferentiates Player.name from the argument "name"
+        }
+
+        public Player(string name, int startingLives)
+        {
+            this.name = name;
+            livesLeft = startingLives;  // "this." isn't needed here, as livesLeft isn't shadowed by anything
+        }
+
+        // Getters
+        public string GetName()
+        {
+            return name;
+        }
+
+        public int GetScore()
+        {
+            return score;
+        }
+
+        public int GetLivesLeft()
+        {
+            return livesLeft;
+        }
+
+
+        // Modifies a variable
+        public void AddPoints(int points)
+        {
+            score += points;
+        }
+
+    }
+}
---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player("Player One");
+
+            player.AddPoints(100);
+
+            Console.WriteLine(player.GetName() + " has " + player.GetScore() + " points");
+
+            // exit prompt
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
