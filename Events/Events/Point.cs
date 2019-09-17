6d96bf0675254a10d8df41e7fd9847c5b2ae04b2
mattsharker <mattsharker@gmail.com>
2019-09-16 18:38:03 -0700

    Added program demonstrating the basics of events

---
+++ Events/Events/Point.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Events
+{
+    class Point
+    {
+        private double x;
+        private double y;
+
+        public double X
+        {
+            get
+            {
+                return x;
+            }
+            set
+            {
+                x = value;
+                OnPointChanged();   // If the event handler is attached, this will raise it
+            }
+        }
+
+        public double Y
+        {
+            get
+            {
+                return y;
+            }
+            set
+            {
+                y = value;
+                OnPointChanged();   // ditto
+            }
+        }
+
+        public event EventHandler PointChanged;
+
+        public void OnPointChanged()
+        {
+            if(PointChanged != null)
+            {
+                PointChanged(this, EventArgs.Empty);
+            }
+        }
+    }
+}
---
+++ Events/Events/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point point = new Point();
+
+            // attaches an event handler
+            point.PointChanged += HandlePointChanged;
+
+            // The event handler will be raised now
+            point.X = 3;
+
+            // detaches an event handler
+            point.PointChanged -= HandlePointChanged;
+
+            // the event handler will not be raised
+            point.Y = 5;
+
+            // exit prompt
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+
+        public static void HandlePointChanged(object sender, EventArgs eventArgs)
+        {
+            Console.WriteLine("The event has been raised");
+        }
+    }
+}
