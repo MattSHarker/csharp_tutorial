b36472a8531f0a497046ddc7a6b902e2eaed573b
mattsharker <mattsharker@gmail.com>
2019-09-16 19:16:07 -0700

    Added program about operator overloading

---
+++ OperatorOverloading/OperatorOverloading/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OperatorOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector a = new Vector(5, 2);
+            Vector b = new Vector(6, 3);
+
+            Vector result = a + b;  // the + overload is used here
+
+            Console.WriteLine("The vector a + vector b: (" + result.X + ", " + result.Y + ")");
+
+            // exit prompt
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OperatorOverloading/OperatorOverloading/Vector.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OperatorOverloading
+{
+    class Vector
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Vector(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        // operator overloads MUST be public and static
+
+        // 
+        public static Vector operator +(Vector v1, Vector v2)
+        {
+            return new Vector(v1.X + v2.X, v1.Y + v2.Y);
+        }
+
+        // unary operators
+        public static Vector operator -(Vector v)   // - as in negative, not minus
+        {
+            return new Vector(-v.X, -v.Y);
+        }
+
+        // operators can have multiple versions
+        public static Vector operator +(Vector v, double scalar)
+        {
+            return new Vector(v.X + scalar, v.Y + scalar);
+        }
+
+        // relational operators can be overloaded too, but they return a bool
+        public static bool operator ==(Vector v1, Vector v2)
+        {
+            return ((v1.X == v2.X) && (v1.Y == v2.Y));
+        }
+
+        // if == is overloaded, != must be overloaded as well
+        public static bool operator !=(Vector v1, Vector v2)
+        {
+            return !(v1 == v2);
+        }
+    }
+}
