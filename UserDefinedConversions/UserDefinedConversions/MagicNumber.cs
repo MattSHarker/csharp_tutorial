72141e122b1e2781e02ef4fc57848d2337cfb109
mattsharker <mattsharker@gmail.com>
2019-09-16 22:03:18 -0700

    Added program about user defined type conversions

---
+++ UserDefinedConversions/UserDefinedConversions/MagicNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UserDefinedConversions
+{
+    class MagicNumber
+    {
+        public int Number { get; set; }
+        public bool IsMagic { get; set; }
+        
+        // converts an int into a MagicNumber
+        // implicit means the conversion does not have to be manually called
+        static public implicit operator MagicNumber(int value)
+        {
+            return new MagicNumber() { Number = value, IsMagic = false };
+        }
+
+        // converts a MagicNumber into an int
+        // when losing information, it's a good idea to make it explicit
+        // explicit ensures you have to explicitly call the conversion
+        static public explicit operator int(MagicNumber magicNumber)
+        {
+            return magicNumber.Number;
+        }
+    }
+}
---
+++ UserDefinedConversions/UserDefinedConversions/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UserDefinedConversions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // converting an int into a MagicNumber
+            int someInt = 5;
+            MagicNumber newMagic = someInt; // the lack of (MagicNumber) in front of
+                                            // someInt is because it is implicit
+
+            // converting a MagicNumber into an int
+            MagicNumber mn = new MagicNumber
+            {
+                Number = 5,
+                IsMagic = true
+            };
+
+            int newInt = (int)mn;   // since this conversion is explicit, the
+                                    // (int) is required to make the conversion
+
+            // exit prompt
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
