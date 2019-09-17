749b1d48cf7266333e28c940111702888d4bdc00
mattsharker <mattsharker@gmail.com>
2019-09-16 18:58:14 -0700

    Added program about the basics of threading

---
+++ Threading/Threading/GenerateNumbersTask.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Threading
+{
+    class GenerateNumbersTask
+    {
+        private static Random random = new Random();
+
+        public int[] Numbers { get; set; }
+
+        public int StartIndex { get; set; }
+        public int Count      { get; set; }
+
+        public void GenerateNumbers()
+        {
+            for (int i = StartIndex; i < StartIndex + Count; ++i)
+                Numbers[i] = random.Next(100);
+        }
+    }
+}
---
+++ Threading/Threading/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Threading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[100000000];
+
+            // Prepare the list of threads and the list of tasks
+            int threadCount = 10;
+            GenerateNumbersTask[] tasks = new GenerateNumbersTask[threadCount];
+            Thread[] threads = new Thread[threadCount];
+
+            // create and start all threads
+            for (int i = 0; i < threadCount; ++i)
+            {
+                tasks[i] = new GenerateNumbersTask()
+                {
+                    Numbers    = numbers,
+                    Count      = 1000,
+                    StartIndex = i * 1000
+                };
+
+                // create the thread
+                threads[i] = new Thread(tasks[i].GenerateNumbers);
+
+                // Start running the threads
+                threads[i].Start();
+            }
+
+            // wait for all threads to finish, and join them
+            for (int i = 0; i < threadCount; ++i)
+                threads[i].Join();
+
+            // exit prompt
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
+
+// to lock a thread, use the "lock" method
+// 
+// private object threadLock = new object();
+// lock(threadLock)
+// {
+//     // code that only one thread should run at a time goes here
+// }
