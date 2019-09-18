dea6b5d1e8cc0c0f51b75067b8f5596303065ded
mattsharker <mattsharker@gmail.com>
2019-09-17 22:32:48 -0700

    Added program on extension methods

---
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // standard string manipulation
+            string message = "I'm sorry, Dave. I'm afraid I can't do that.";
+            Console.WriteLine("Original: " + message);
+
+            // the extended method works the same way as any other string method
+            Console.WriteLine("Random:   " + message.ToRandomCase());
+
+            // this is actually what is happening in the above line...
+            StringExtension.ToRandomCase(message);
+            // ... but because the class was extended, we can use first syntax
+
+            // exit prompt
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ExtensionMethods/ExtensionMethods/StringExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+// an extension method allows the user to treat the method as if
+// it is part of the original class it is extending
+namespace ExtensionMethods
+{
+    // a class with an extension must be static
+    public static class StringExtension
+    {
+        // this needs to be static if it's in a static class
+        private static Random random = new Random();
+
+        // all methods in a static class need to be static as well
+        // the "this" keyword turns the method into an extension
+        public static string ToRandomCase(this string text)
+        {
+            string result = "";
+
+            for (int i = 0; i < text.Length; ++i)
+            {
+                if (random.Next(2) == 0)
+                    result += text.Substring(i, 1).ToUpper();
+                else
+                    result += text.Substring(i, 1).ToLower();
+            }
+
+            return result;
+        }
+    }
+}
